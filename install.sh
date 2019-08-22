81203b2c74f9122dc5f0f3272a46848279daf432
ginfo <adornetejr@gmail.com>
2019-08-22 16:02:05 -0300

    Update install script

--- install.sh
+++ install.sh
@@ -5,7 +5,10 @@ git clone https://github.com/adornetejr/dataverse-furg
 wget http://download.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 rpm -ihv epel-release*
 yum update
-yum install htop wget java-1.8.0-openjdk java-1.8.0-openjdk-devel unzip firewalld nano
+yum install htop wget java-1.8.0-openjdk java-1.8.0-openjdk-devel unzip nmap nano
+cd /etc/
+rm hosts
+wget https://github.com/adornetejr/dataverse-furg/blob/master/hosts
 wget https://releases.hashicorp.com/vagrant/2.2.5/vagrant_2.2.5_x86_64.rpm
 wget https://github.com/IQSS/dataverse/archive/v4.9.1.zip
 unzip v4.9.1.zip
@@ -20,6 +23,7 @@ cd /usr/local/glassfish4/glassfish/modules
 rm weld-osgi-bundle.jar
 wget http://central.maven.org/maven2/org/jboss/weld/weld-osgi-bundle/2.2.10.SP1/weld-osgi-bundle-2.2.10.SP1-glassfish4.jar
 cd /usr/local/glassfish4/glassfish/domains/domain1/config/
+rm domain.xml
 wget https://github.com/adornetejr/dataverse-furg/blob/master/domain.xml
 /usr/local/glassfish4/bin/asadmin start-domain
 /usr/local/glassfish4/bin/asadmin osgi lb | grep 'Weld OSGi Bundle'
