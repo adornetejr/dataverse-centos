198886afa02b376b303eb5715d8e4c7a95949d8e
Adornete Jr <adornetejr@gmail.com>
2019-09-03 18:16:51 -0300

    Update script

--- install.sh
+++ install.sh
@@ -1,38 +1,48 @@
-########
-rm -f anaconda-ks.cfg
-yum update
-yum install wget git net-tools
+#!/bin/bash
+cd
+rm -f anaconda-ks.cfg epel-release-latest-7.noarch.rpm
+yum update -y
+yum install -y wget git net-tools
+rm -rf dataverse-furg
 git clone https://github.com/adornetejr/dataverse-furg
 wget http://download.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 rpm -ihv epel-release*
-yum update
-yum install nano curl htop lynx wget unzip nmap httpd mod_ssl lsof java-1.8.0-openjdk java-1.8.0-openjdk-devel ImageMagick sendmail sendmail-cf m4 R
+yum update -y
+yum install -y nano curl htop lynx wget unzip nmap httpd mod_ssl lsof java-1.8.0-openjdk java-1.8.0-openjdk-devel ImageMagick sendmail sendmail-cf m4 R
 cd /etc/
 rm -f hosts
 wget https://raw.githubusercontent.com/adornetejr/dataverse-furg/master/hosts
-reboot
-########
+cd
+rm -rf vagrant_2.2.5_x86_64.rpm v4.9.1.zip dataverse-4.9.1.war dvinstall.zip
 wget https://releases.hashicorp.com/vagrant/2.2.5/vagrant_2.2.5_x86_64.rpm
 wget https://github.com/IQSS/dataverse/archive/v4.9.1.zip
 wget https://github.com/IQSS/dataverse/releases/download/v4.9.1/dataverse-4.9.1.war
 wget https://github.com/IQSS/dataverse/releases/download/v4.9.1/dvinstall.zip
+rm -rf dataverse-4.9.1
 unzip v4.9.1.zip
+rm -rf dvinstall
 unzip dvinstall.zip
 cp -R dvinstall /tmp/
 cd dataverse-4.9.1/downloads
 ./download.sh
-cd ../..
+cd
+rm -rf glassfish-4.1.zip
 wget http://dlc-cdn.sun.com/glassfish/4.1/release/glassfish-4.1.zip
 unzip glassfish-4.1.zip
+rm -rf /usr/local/glassfish4
 mv glassfish4 /usr/local
-chown -R root:root /usr/local/glassfish4
+useradd glassfish
+chown -R glassfish:glassfish /usr/local/glassfish4
+su - glassfish
 cd /usr/local/glassfish4/glassfish/modules
 rm -f weld-osgi-bundle.jar
 wget http://central.maven.org/maven2/org/jboss/weld/weld-osgi-bundle/2.2.10.SP1/weld-osgi-bundle-2.2.10.SP1-glassfish4.jar
 cd /usr/local/glassfish4/glassfish/domains/domain1/config/
 rm -f domain.xml
 wget https://raw.githubusercontent.com/adornetejr/dataverse-furg/master/domain.xml
+rm -rf /usr/local/glassfish4/glassfish/domains/domain1/config/cacerts.jks
 cp -f /usr/lib/jvm/java-1.8.0-openjdk/jre/lib/security/cacerts /usr/local/glassfish4/glassfish/domains/domain1/config/cacerts.jks
+exit
 cd /usr/lib/systemd/system
 rm -f glassfish.service
 wget https://raw.githubusercontent.com/adornetejr/dataverse-furg/master/glassfish.service
@@ -40,10 +50,14 @@ systemctl daemon-reload
 systemctl start glassfish.service
 systemctl enable glassfish.service
 cd /usr/bin
+rm -rf jq
 wget http://stedolan.github.io/jq/download/linux64/jq
 chmod +x jq
 jq --version
-yum install -y https://download.postgresql.org/pub/repos/yum/9.6/redhat/rhel-7-x86_64/pgdg-centos96-9.6-3.noarch.rpm
+cd
+rm -rf pgdg-centos96-9.6-3.noarch.rpm
+wget https://download.postgresql.org/pub/repos/yum/9.6/redhat/rhel-7-x86_64/pgdg-centos96-9.6-3.noarch.rpm
+yum install -y pgdg-centos96-9.6-3.noarch.rpm
 yum makecache fast
 yum install -y postgresql96-server
 /usr/pgsql-9.6/bin/postgresql96-setup initdb
@@ -107,13 +121,28 @@ cd /tmp/dvinstall
 wget https://raw.githubusercontent.com/adornetejr/dataverse-furg/master/default.config
 ./install
 
+cd /usr/local/glassfish4/glassfish/domains/domain1/applications/dataverse/WEB-INF/classes/
+rm -rf *.properties
+wget https://raw.githubusercontent.com/adornetejr/dataverse-furg/master/Bundle.properties
+wget https://raw.githubusercontent.com/adornetejr/dataverse-furg/master/MimeTypeDisplay.properties
+wget https://raw.githubusercontent.com/adornetejr/dataverse-furg/master/MimeTypeFacets.properties
+systemctl restart glassfish.service
+
+
+
+
+
 #########
+cd
 sudo -i R
-install.packages("R2HTML", repos="https://cloud.r-project.org/",lib="/usr/lib/R/library" )
-install.packages("rjson", repos="https://cloud.r-project.org/",lib="/usr/lib/R/library" )
-install.packages("DescTools", repos="https://cloud.r-project.org/",lib="/usr/lib/R/library" )
-install.packages("Rserve", repos="https://cloud.r-project.org/",lib="/usr/lib/R/library" )
-install.packages("haven", repos="https://cloud.r-project.org/",lib="/usr/lib/R/library" )
+dir.create(Sys.getenv("R_LIBS_USER"), recursive = TRUE) 
+.libPaths(Sys.getenv("R_LIBS_USER")) 
+install.packages("R2HTML", repos="https://cloud.r-project.org/" )
+install.packages("rjson", repos="https://cloud.r-project.org/" )
+install.packages("DescTools", repos="https://cloud.r-project.org/" )
+install.packages("Rserve", repos="https://cloud.r-project.org/" )
+install.packages("haven", repos="https://cloud.r-project.org/" )
+q()
 #########
 su - postgres
 psql
@@ -129,30 +158,3 @@ systemctl restart glassfish.service
 senha==>'gfginfo2019'
 #########
 /usr/local/glassfish4/bin/asadmin list-applications
-
-
-
-
-
-systemctl status glassfish.service
-systemctl status postgresql-9.6
-
-systemctl status glassfish.service
-systemctl status glassfish.service
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
