d0c42f794909e277cbdffb7391b259bb4749a801
Adornete Jr <adornetejr@gmail.com>
2019-09-10 17:24:54 -0300

    Update bundle l260

--- translate.sh
+++ translate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
+systemctl stop glassfish.service
 cd /usr/local/glassfish4/glassfish/domains/domain1/applications/dataverse/WEB-INF/classes/
 rm -rf Bundle.properties
 wget https://raw.githubusercontent.com/adornetejr/dataverse-furg/master/Bundle.properties
-systemctl restart glassfish.service
-systemctl restart glassfish.service
+systemctl start glassfish.service
