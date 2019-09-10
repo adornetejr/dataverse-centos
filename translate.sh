53448cde17e5b7c029c8f009565606d6acbc61ab
Adornete Jr <adornetejr@gmail.com>
2019-09-10 17:42:53 -0300

    Update translate

--- translate.sh
+++ translate.sh
@@ -4,3 +4,7 @@ cd /usr/local/glassfish4/glassfish/domains/domain1/applications/dataverse/WEB-IN
 rm -rf Bundle.properties
 wget https://raw.githubusercontent.com/adornetejr/dataverse-furg/master/Bundle.properties
 systemctl start glassfish.service
+
+
+
+curl http://localhost:8080/api/admin/settings/:Languages -X PUT -d '[{"locale":"en_US","title":"English"},{"locale":"pt_BR","title":"Português"}]'
