75473a6b04ddd820a264f1d5fb4fdd83e864d19c
Adornete Jr <adornetejr@gmail.com>
2020-02-03 09:16:20 -0300

    Remove jq.sh

--- jq.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# INSTALA DEPENDENCIA JQ
-rm -rf /usr/bin/jq
-cd /usr/bin
-wget http://stedolan.github.io/jq/download/linux64/jq
-chmod +x /usr/bin/jq
-echo "Checking JQ version!"
-jq --version
