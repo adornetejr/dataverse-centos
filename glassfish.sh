f74c746e2dfd75db98bfd46cd870af9827f9f6ac
Adornete Jr <adornetejr@gmail.com>
2020-02-04 16:23:52 -0300

    Update glassfish

--- glassfish.sh
+++ glassfish.sh
@@ -21,7 +21,8 @@ mv glassfish4 /usr/local/
 cd /usr/local/glassfish4/glassfish/modules
 # ATUALIZA MODULO WELD-OSGI
 rm -f weld-osgi-bundle.jar
-wget http://central.maven.org/maven2/org/jboss/weld/weld-osgi-bundle/2.2.10.SP1/weld-osgi-bundle-2.2.10.SP1-glassfish4.jar
+curl -L -O https://search.maven.org/remotecontent?filepath=org/jboss/weld/weld-osgi-bundle/2.2.10.Final/weld-osgi-bundle-2.2.10.Final-glassfish4.jar
+# wget http://central.maven.org/maven2/org/jboss/weld/weld-osgi-bundle/2.2.10.SP1/weld-osgi-bundle-2.2.10.SP1-glassfish4.jar
 # CONFIGURA GLASSFISH DE CLIENTE PARA SERVIDOR
 cd /usr/local/glassfish4/glassfish/domains/domain1/config/
 rm -f domain.xml
@@ -31,7 +32,10 @@ rm -rf /usr/local/glassfish4/glassfish/domains/domain1/config/cacerts.jks
 cp -f /usr/lib/jvm/java-1.8.0-openjdk/jre/lib/security/cacerts /usr/local/glassfish4/glassfish/domains/domain1/config/cacerts.jks
 # ALTERA PERMICOES PARA USUARIO glassfish
 useradd glassfish
-chown -R glassfish:glassfish /usr/local/glassfish4
+chown -R root:root /usr/local/glassfish4
+chown glassfish /usr/local/glassfish4/glassfish/lib
+chown -R glassfish:glassfish /usr/local/glassfish4/glassfish/domains/domain1
+# chown -R glassfish:glassfish /usr/local/glassfish4
 # ATIVA SERVICO GLASSFISH PARA INICIALIZAR COM SISTEMA
 rm -f /usr/lib/systemd/system/glassfish.service
 cp $DIR/glassfish.service /usr/lib/systemd/system/
