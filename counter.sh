1005f68f5d9b0c3b632aad1a5c2e2985e5a5c8c8
Adornete Jr <adornetejr@gmail.com>
2020-02-16 01:48:21 -0300

    Update if md5sum

--- counter.sh
+++ counter.sh
@@ -4,23 +4,25 @@ DIR=$PWD
 counter="/tmp/v0.0.1.tar.gz"
 link=https://github.com/CDLUC3/counter-processor/archive/v0.0.1.tar.gz
 cd /tmp/
-if [ -f "$counter" ]
-then
+rm -rf counter-processor-0.0.1
+if [ -f "$counter" ]; then
     ls $counter
-    md5sum $counter
+    if [ "$(md5sum $counter)" == "de4f375f0c68c404e8adc52092cb8334  /tmp/v0.0.1.tar.gz" ]; then
+        tar xvfz v0.0.1.tar.gz
+    fi
 else
+    rm $counter
     wget $link
+    tar xvfz v0.0.1.tar.gz
 fi
-rm -rf counter-processor-0.0.1 /usr/local/counter-processor-0.0.1
-tar xvfz v0.0.1.tar.gz
-cp -R counter-processor-0.0.1 /usr/local
-cd /usr/local/counter-processor-0.0.1
+cp -R counter-processor-0.0.1 /usr/local/counter-processor
+cd /usr/local/counter-processor
 cp $PWD/GeoLite2-Country GeoLite2-Country.tar.gz
 tar xvfz GeoLite2-Country.tar.gz
 cp GeoLite2-Country_*/GeoLite2-Country.mmdb maxmind_geoip
 # ADICIONA USUARIO COUNTER
 useradd counter
-chown -R counter:counter /usr/local/counter-processor-0.0.1
+chown -R counter:counter /usr/local/counter-processor
 python3.6 -m ensurepip
-cd /usr/local/counter-processor-0.0.1
+cd /usr/local/counter-processor
 pip3 install -r requirements.txt
