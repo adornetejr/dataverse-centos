769c904c7e413be2ab1ffa0f3352e7fad63373ff
Adornete Jr <adornetejr@gmail.com>
2020-02-16 05:46:42 -0300

    Update apache

--- apache.sh
+++ apache.sh
@@ -2,13 +2,25 @@
 DIR=$PWD
 systemctl stop httpd
 yum remove -y httpd mod_ssl
-rm -rf /etc/httpd
 yum install -y httpd mod_ssl
-echo "LoadModule rewrite_module modules/mod_rewrite.so" >> /etc/httpd/conf.modules.d/00-base.conf
-sed 's/localdomain/'$HOSTNAME'/g' localdomain.conf > APACHE
-echo $APACHE
-rm -rf /etc/httpd/conf.d/080-default.conf
-cp 080-default.conf /etc/httpd/conf.d
+systemctl stop httpd
+rm -rf etc/httpd/conf.d/dataverse.conf
+cp $DIR/dataverse.conf /etc/httpd/conf.d
+rm -rf etc/httpd/conf.d/ssl.conf
+cp $DIR/ssl.conf /etc/httpd/conf.d
+rm -rf etc/httpd/conf/httpd.conf
+cp $DIR/httpd.conf /etc/conf
+mkdir /etc/httpd/ssl
+rm -rf /etc/httpd/ssl/root.furg.br.cer /etc/httpd/ssl/furg.br.cer /etc/httpd/ssl/$HOSTNAME.cer /etc/httpd/ssl/$HOSTNAME.key
+cp $DIR/furg.br.cer /etc/httpd/ssl
+cp $DIR/root.furg.br.cer /etc/httpd/ssl
+chown root:root /etc/httpd/ssl/furg.br.cer
+chmod 600 /etc/httpd/ssl/furg.br.cer
+cd $DIR
+chmod 744 keygen.sh
+./keygen.sh -y 3 -f -u root -g root -h dataverse.c3.furg.br -e https://dataverse.c3.furg.br/
+mv sp-cert.pem /etc/httpd/ssl/$HOSTNAME.cer
+mv sp-key.pem /etc/httpd/ssl/$HOSTNAME.key
 systemctl stop httpd
 echo "Starting httpd!"
 systemctl enable httpd
