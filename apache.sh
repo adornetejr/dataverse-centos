dbc73698de0f5b98902984805e4f07917fcd702c
Adornete Jr <adornetejr@gmail.com>
2020-02-16 04:50:24 -0300

    Update apache

--- apache.sh
+++ apache.sh
@@ -5,7 +5,7 @@ yum remove -y httpd mod_ssl
 rm -rf /etc/httpd
 yum install -y httpd mod_ssl
 echo "LoadModule rewrite_module modules/mod_rewrite.so" >> /etc/httpd/conf.modules.d/00-base.conf
-sed 's/localdomain/'$HOSTNAME'/g' 080-default.conf > APACHE
+sed 's/localdomain/'$HOSTNAME'/g' localdomain.conf > APACHE
 echo $APACHE
 rm -rf /etc/httpd/conf.d/080-default.conf
 cp 080-default.conf /etc/httpd/conf.d
