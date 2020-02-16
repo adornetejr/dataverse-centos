3593628e67a537ee2a69c291db1f05c75a02707c
Adornete Jr <adornetejr@gmail.com>
2020-02-16 03:58:39 -0300

    Update apache

--- apache.sh
+++ apache.sh
@@ -7,6 +7,9 @@ yum install -y httpd mod_ssl
 sed 's/localdomain/'$HOSTNAME'/g' 080-default.conf > 080-default.conf
 rm -rf /etc/httpd/conf.d/080-default.conf
 cp 080-default.conf /etc/httpd/conf.d
+sed 's/localdomain/'$HOSTNAME'/g' 443-default.conf > 443-default.conf
+rm -rf /etc/httpd/conf.d/443-default.conf
+cp 443-default.conf /etc/httpd/conf.d
 echo "Starting httpd!"
 systemctl enable httpd
 systemctl start httpd
