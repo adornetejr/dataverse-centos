e49699659f8b7c99406b6096bcec506406779ee7
Adornete Jr <adornetejr@gmail.com>
2020-05-07 04:57:26 -0300

    Fix: sendmail test

--- sh/sendmail.sh
+++ sh/sendmail.sh
@@ -35,12 +35,14 @@ systemctl restart sendmail
 echo "ADMIN_EMAIL	admin@$HOSTNAME" >> default.config
 echo "MAIL_SERVER	127.0.0.1" >> default.config
 echo "Starting sendmail!"
-echo "From: $USER@$(hostname --fqdn)" >> $DIR/mail/mail.txt
+HOST=$(hostname --fqdn)
+echo "From: $USER@$HOST" >> $DIR/mail/mail.txt
 echo " " >> $DIR/mail/mail.txt
 echo "Servidor:" >> $DIR/mail/mail.txt
-echo $(hostname --fqdn) >> $DIR/mail/mail.txt
+echo $HOST >> $DIR/mail/mail.txt
 echo "Hora:" >> $DIR/mail/mail.txt
-echo $(date) >> $DIR/mail/mail.txt
+TIME=$(date)
+echo $TIME >> $DIR/mail/mail.txt
 systemctl enable sendmail
 systemctl start sendmail
 # EMAIL TEST DO SENDMAIL
