f4f92b0309fdac1ce45a7d6e9cb0beadb48b4923
Adornete Jr <adornetejr@gmail.com>
2020-05-12 00:59:06 -0300

    Fix: sendmail config

--- sh/sendmail.sh
+++ sh/sendmail.sh
@@ -30,13 +30,14 @@ hostname > /etc/mail/local-host-names
 hostname > /etc/mail/relay-domains
 mv /etc/mail/sendmail.mc /etc/mail/sendmail.mc.1.bkp
 sed '/^$/d' $DIR/mail/sendmail.mc
+HOST=$(hostname --fqdn)
+sed "s/dataverse.c3.furg.br/$HOST/g" $DIR/mail/sendmail.mc
 cp $DIR/mail/sendmail.mc /etc/mail/sendmail.mc
 m4 /etc/mail/sendmail.mc > /etc/mail/sendmail.cf
 systemctl restart sendmail
 echo "ADMIN_EMAIL	admin@$HOSTNAME" >> $DIR/default.config
 echo "MAIL_SERVER	127.0.0.1" >> $DIR/default.config
 echo "Starting sendmail!"
-HOST=$(hostname --fqdn)
 echo "From: $USER@$HOST" >> $DIR/mail/mail.txt
 echo " " >> $DIR/mail/mail.txt
 echo "Servidor:" >> $DIR/mail/mail.txt
