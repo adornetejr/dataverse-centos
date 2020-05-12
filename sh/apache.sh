2ba743ecbf4f284e8c0cf69b14ae44ddd1f3df9e
Adornete Jr <adornetejr@gmail.com>
2020-05-12 19:06:30 -0300

    Fix: apache ssl certificates

--- sh/apache.sh
+++ sh/apache.sh
@@ -3,7 +3,6 @@ DIR=$PWD
 RED=`tput setaf 1`
 GREEN=`tput setaf 2`
 RESET=`tput sgr0`
-HOST=$(hostname --fqdn)
 echo "${GREEN}Stopping Apache!${RESET}"
 systemctl stop httpd
 echo "${GREEN}Removing old settings!${RESET}"
@@ -12,19 +11,21 @@ echo "${GREEN}Installing Apache!${RESET}"
 yum install -y httpd mod_ssl
 systemctl stop httpd
 echo "${GREEN}Setting up Apache!${RESET}"
+HOST=$(hostname --fqdn)
 mv /etc/httpd/conf.d/$HOST.conf /etc/httpd/conf.d/$HOST.conf.bkp
 sed "s/dataverse.c3.furg.br/$HOST/g" $DIR/conf/dataverse.conf >/etc/httpd/conf.d/$HOST.conf
 mv etc/httpd/conf.d/ssl.conf etc/httpd/conf.d/ssl.conf.bkp
 sed "s/dataverse.c3.furg.br/$HOST/g" $DIR/conf/ssl.conf >/etc/httpd/conf.d/ssl.conf
-cp $DIR/conf/ssl.conf /etc/httpd/conf.d
 mv /etc/httpd/conf/httpd.conf /etc/httpd/conf/httpd.conf.bkp
 sed "s/dataverse.c3.furg.br/$HOST/g" $DIR/conf/httpd.conf >/etc/httpd/conf/httpd.conf
+echo "${GREEN}Backing up SSL Certificates!${RESET}"
 mv /etc/httpd/ssl /etc/httpd/ssl-bkp
 mkdir /etc/httpd/ssl
 cp $DIR/cert/chain.$HOST.cer /etc/httpd/ssl
 cp $DIR/cert/root.$HOST.cer /etc/httpd/ssl
 chown root:root /etc/httpd/ssl/*
 chmod 600 /etc/httpd/ssl/*
+echo "${GREEN}Generating new SSL Certificates!${RESET}"
 cd $DIR/cert
 ./keygen.sh -y 3 -f -u root -g root -h $HOST -e https://$HOST/
 mv sp-cert.pem /etc/httpd/ssl/$HOST.cer
