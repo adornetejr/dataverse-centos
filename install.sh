bc34be8ec369117cc57a246e05caf05255078dd2
Adornete Jr <adornetejr@gmail.com>
2020-05-12 16:15:56 -0300

    Fix: better inteface

--- install.sh
+++ install.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 DIR=$PWD
-echo "Removing old settings!"
+G=`tput setaf 2`
+echo "${G}Removing old settings!"
 mv $DIR/default.config $DIR/default.config.bkp
 mv $DIR/logs/install.out $DIR/logs/install.out.bkp
 mv $DIR/logs/install.err $DIR/logs/install.err.bkp
 # SERVICE FIREWALLD STOP
 systemctl stop firewalld
 # INSTALL FEDORA REPOSITORY
-echo "Installing Fedora repository!"
+echo "${G}Installing Fedora repository!"
 yum install -y epel-release
 # UPDATE PACKAGES
 echo "Updating installed packages!"
@@ -19,54 +20,63 @@ cd $DIR
 chmod 744 sh/*.sh
 sh/sendmail.sh
 echo " "
+echo "Sendmail installed!"
 echo "Stage (1/10) done!"
 echo "Ctrl+C to cancel or Enter to continue!"
 read -e $X
 sh/glassfish.sh
 echo " "
-echo "Stage (1/10) done!"
+echo "Glassfish installed!"
+echo "Stage (2/10) done!"
 echo "Ctrl+C to cancel or Enter to continue!"
 read -e $X
 cd $DIR
 sh/solr.sh
 echo " "
-echo "Stage (2/10) done!"
+echo "Solr installed!"
+echo "Stage (3/10) done!"
 echo "Ctrl+C to cancel or Enter to continue!"
 read -e $X
 cd $DIR
 sh/postgresql.sh
 echo " "
-echo "Stage (3/10) done!"
+echo "Postgres installed!"
+echo "Stage (4/10) done!"
 echo "Ctrl+C to cancel or Enter to continue!"
 read -e $X
 cd $DIR
 sh/rserve.sh
 echo " "
-echo "Stage (4/10) done!"
+echo "Rserve installed!"
+echo "Stage (5/10) done!"
 echo "Ctrl+C to cancel or Enter to continue!"
 read -e $X
 cd $DIR
 sh/counter.sh
 echo " "
-echo "Stage (5/10) done!"
+echo "Counter-Processor installed!"
+echo "Stage (6/10) done!"
 echo "Ctrl+C to cancel or Enter to continue!"
 read -e $X
 cd $DIR
 sh/dataverse.sh
 echo " "
-echo "Stage (6/10) done!"
+echo "Dataverse installed!"
+echo "Stage (7/10) done!"
 echo "Ctrl+C to cancel or Enter to continue!"
 read -e $X
 cd $DIR
 sh/apache.sh
 echo " "
-echo "Stage (7/10) done!"
+echo "Apache installed!"
+echo "Stage (8/10) done!"
 echo "Ctrl+C to cancel or Enter to continue!"
 read -e $X
 cd $DIR
 sh/shibboleth.sh
 echo " "
-echo "Stage (8/10) done!"
+echo "Shibboleth installed!"
+echo "Stage (9/10) done!"
 echo "Ctrl+C to cancel or Enter to continue!"
 read -e $X
 cd $DIR
@@ -74,7 +84,9 @@ sh/firewalld.sh
 rm -rf $DIR/default.config /tmp/dvinstall/default.config
 clear
 echo " "
-echo "Stage (9/10) done!"
+echo "Firewalld installed!"
+echo "Stage (10/10) done!"
+echo " "
 echo "Installation completed!"
 echo " "
 echo "Attention!!"
