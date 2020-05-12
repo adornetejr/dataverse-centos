8c074096550be58bd2f40e990d317ae9e48e807d
Adornete Jr <adornetejr@gmail.com>
2020-05-12 16:33:43 -0300

    Fix: better interface

--- install.sh
+++ install.sh
@@ -1,82 +1,82 @@
 #!/bin/bash
 DIR=$PWD
-G=`tput setaf 2`
+GREEN=`tput setaf 2`
 RESET=`tput sgr0`
-echo "${G}Removing old settings!${RESET}"
+echo "${GREEN}Removing old settings!${RESET}"
 mv $DIR/default.config $DIR/default.config.bkp
 mv $DIR/logs/install.out $DIR/logs/install.out.bkp
 mv $DIR/logs/install.err $DIR/logs/install.err.bkp
 # SERVICE FIREWALLD STOP
 systemctl stop firewalld
 # INSTALL FEDORA REPOSITORY
-echo "${G}Installing Fedora repository!${RESET}"
+echo "${GREEN}Installing Fedora repository!${RESET}"
 yum install -y epel-release
 # UPDATE PACKAGES
-echo "Updating installed packages!"
+echo "${GREEN}Updating installed packages!${RESET}"
 yum update -y
 # INSTALL RECOMMENDED PACKAGES
-echo "Installing dependencies!"
+echo "${GREEN}Installing dependencies!${RESET}"
 yum install -y nano htop wget git net-tools lynx unzip curl
 cd $DIR
 chmod 744 sh/*.sh
 sh/sendmail.sh
 echo " "
-echo "Sendmail installed!"
+echo "${GREEN}Sendmail installed!${RESET}"
 echo "Stage (1/10) done!"
 echo "Ctrl+C to cancel or Enter to continue!"
 read -e $X
 sh/glassfish.sh
 echo " "
-echo "Glassfish installed!"
+echo "${GREEN}Glassfish installed!${RESET}"
 echo "Stage (2/10) done!"
 echo "Ctrl+C to cancel or Enter to continue!"
 read -e $X
 cd $DIR
 sh/solr.sh
 echo " "
-echo "Solr installed!"
+echo "${GREEN}Solr installed!${RESET}"
 echo "Stage (3/10) done!"
 echo "Ctrl+C to cancel or Enter to continue!"
 read -e $X
 cd $DIR
 sh/postgresql.sh
 echo " "
-echo "Postgres installed!"
+echo "${GREEN}Postgres installed!${RESET}"
 echo "Stage (4/10) done!"
 echo "Ctrl+C to cancel or Enter to continue!"
 read -e $X
 cd $DIR
 sh/rserve.sh
 echo " "
-echo "Rserve installed!"
+echo "${GREEN}Rserve installed!${RESET}"
 echo "Stage (5/10) done!"
 echo "Ctrl+C to cancel or Enter to continue!"
 read -e $X
 cd $DIR
 sh/counter.sh
 echo " "
-echo "Counter-Processor installed!"
+echo "${GREEN}Counter-Processor installed!${RESET}"
 echo "Stage (6/10) done!"
 echo "Ctrl+C to cancel or Enter to continue!"
 read -e $X
 cd $DIR
 sh/dataverse.sh
 echo " "
-echo "Dataverse installed!"
+echo "${GREEN}Dataverse installed!${RESET}"
 echo "Stage (7/10) done!"
 echo "Ctrl+C to cancel or Enter to continue!"
 read -e $X
 cd $DIR
 sh/apache.sh
 echo " "
-echo "Apache installed!"
+echo "${GREEN}Apache installed!${RESET}"
 echo "Stage (8/10) done!"
 echo "Ctrl+C to cancel or Enter to continue!"
 read -e $X
 cd $DIR
 sh/shibboleth.sh
 echo " "
-echo "Shibboleth installed!"
+echo "${GREEN}Shibboleth installed!${RESET}"
 echo "Stage (9/10) done!"
 echo "Ctrl+C to cancel or Enter to continue!"
 read -e $X
@@ -85,14 +85,14 @@ sh/firewalld.sh
 rm -rf $DIR/default.config /tmp/dvinstall/default.config
 clear
 echo " "
-echo "Firewalld installed!"
+echo "${GREEN}Firewalld installed!${RESET}"
 echo "Stage (10/10) done!"
 echo " "
 echo "Installation completed!"
 echo " "
-echo "Attention!!"
+echo "${GREEN}Attention!!"
 echo "Faça a relação de confiança para o login federado funcionar!"
 echo " "
 echo "Saiba mais em http://hdl.handle.net/20.500.11959/1264"
-echo " "
+echo "${RESET}"
 read -e $X
