97ea1185876994c4d0039baf881473b51a56ed5b
Adornete Jr <adornetejr@gmail.com>
2020-05-12 19:12:33 -0300

    Fix: apache ssl certificates

--- sh/apache.sh
+++ sh/apache.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 DIR=$PWD
-RED=`tput setaf 1`
-GREEN=`tput setaf 2`
-RESET=`tput sgr0`
+RED=$(tput setaf 1)
+GREEN=$(tput setaf 2)
+RESET=$(tput sgr0)
 echo "${GREEN}Stopping Apache!${RESET}"
 systemctl stop httpd
+echo "${GREEN}Backing up SSL Certificates!${RESET}"
+mv /etc/httpd/ssl /etc/httpd/ssl-bkp
+mkdir /etc/httpd/ssl
+if [ -f "$DIR/cert/chain.$HOST.cer" ]; then
+  cp $DIR/cert/chain.$HOST.cer /etc/httpd/ssl
+else
+  sed "s/'SSLCertificateChainFile /etc'/'# SSLCertificateChainFile /etc'/g" $DIR/conf/ssl.conf >$DIR/conf/ssl.conf
+fi
+if [ -f "$DIR/cert/root.$HOST.cer" ]; then
+  cp $DIR/cert/root.$HOST.cer /etc/httpd/ssl
+else
+  sed "s/'SSLCACertificateFile /etc'/'# SSLCACertificateFile /etc'/g" $DIR/conf/ssl.conf >$DIR/conf/ssl.conf
+fi
 echo "${GREEN}Removing old settings!${RESET}"
 yum remove -y httpd mod_ssl
 echo "${GREEN}Installing Apache!${RESET}"
@@ -14,15 +27,10 @@ echo "${GREEN}Setting up Apache!${RESET}"
 HOST=$(hostname --fqdn)
 mv /etc/httpd/conf.d/$HOST.conf /etc/httpd/conf.d/$HOST.conf.bkp
 sed "s/dataverse.c3.furg.br/$HOST/g" $DIR/conf/dataverse.conf >/etc/httpd/conf.d/$HOST.conf
-mv etc/httpd/conf.d/ssl.conf etc/httpd/conf.d/ssl.conf.bkp
-sed "s/dataverse.c3.furg.br/$HOST/g" $DIR/conf/ssl.conf >/etc/httpd/conf.d/ssl.conf
 mv /etc/httpd/conf/httpd.conf /etc/httpd/conf/httpd.conf.bkp
 sed "s/dataverse.c3.furg.br/$HOST/g" $DIR/conf/httpd.conf >/etc/httpd/conf/httpd.conf
-echo "${GREEN}Backing up SSL Certificates!${RESET}"
-mv /etc/httpd/ssl /etc/httpd/ssl-bkp
-mkdir /etc/httpd/ssl
-cp $DIR/cert/chain.$HOST.cer /etc/httpd/ssl
-cp $DIR/cert/root.$HOST.cer /etc/httpd/ssl
+mv etc/httpd/conf.d/ssl.conf etc/httpd/conf.d/ssl.conf.bkp
+sed "s/dataverse.c3.furg.br/$HOST/g" $DIR/conf/ssl.conf >/etc/httpd/conf.d/ssl.conf
 chown root:root /etc/httpd/ssl/*
 chmod 600 /etc/httpd/ssl/*
 echo "${GREEN}Generating new SSL Certificates!${RESET}"
