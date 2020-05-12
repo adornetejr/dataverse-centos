b01ec383e35d30b1affea5bae460e196a971ba46
Adornete Jr <adornetejr@gmail.com>
2020-05-12 09:30:00 -0300

    Fix: postgresl pg_hba

--- sh/dataverse.sh
+++ sh/dataverse.sh
@@ -12,11 +12,23 @@ read -e $X
 mv /tmp/dvinstall/default.config /tmp/dvinstall/default.config.bkp
 cp $DIR/default.config /tmp/dvinstall/default.config
 clear
+echo "Dataverse Install Settings:"
+echo " "
 cat /tmp/dvinstall/default.config
+echo " "
+echo "Ctrl+C to cancel or Enter to continue!"
+read -e $X
 # START INSTALLER
 cd /tmp/dvinstall/
-sudo -S -u glassfish ./install -y -f > install.out 2> install.err
+echo " "
+echo "Wait... Installing."
+sudo -S -u glassfish ./install -y -f > $DIR/install.out 2> $DIR/install.err
+echo "Install log file $DIR/install.out"
 # SETTING UP POSTGRES ACCESS
 systemctl stop postgresql-9.6
 rm -rf /var/lib/pgsql/9.6/data/pg_hba.conf
 cp $DIR/conf/pg_hba_md5.conf /var/lib/pgsql/9.6/data/pg_hba.conf
+systemctl start postgresql-9.6
+# RESTARTING GLASSFISH
+systemctl stop glassfish
+systemctl start glassfish
