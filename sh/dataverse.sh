db6c42e7df9a9cb04ed3e249df5c8857eabfb4ff
Adornete Jr <adornetejr@gmail.com>
2020-05-12 09:39:05 -0300

    Fix: dataverse

--- sh/dataverse.sh
+++ sh/dataverse.sh
@@ -25,10 +25,12 @@ echo "Wait... Installing."
 sudo -S -u glassfish ./install -y -f > $DIR/install.out 2> $DIR/install.err
 echo "Install log file $DIR/install.out"
 # SETTING UP POSTGRES ACCESS
+echo "Restart Postgresql"
 systemctl stop postgresql-9.6
 rm -rf /var/lib/pgsql/9.6/data/pg_hba.conf
 cp $DIR/conf/pg_hba_md5.conf /var/lib/pgsql/9.6/data/pg_hba.conf
 systemctl start postgresql-9.6
 # RESTARTING GLASSFISH
+echo "Restart Glassfish"
 systemctl stop glassfish
 systemctl start glassfish
