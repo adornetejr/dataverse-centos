f231b7723fa02eb415e358171d3f1d3d10fe37ad
Adornete Jr <adornetejr@gmail.com>
2020-05-13 00:22:54 -0300

    Fix: apache https

--- sh/apache.sh
+++ sh/apache.sh
@@ -14,7 +14,6 @@ echo "${GREEN}Installing Apache!${RESET}"
 yum install -y httpd mod_ssl
 systemctl stop httpd
 echo "${GREEN}Setting up Apache!${RESET}"
-mkdir /etc/httpd/ssl
 HOST=$(hostname --fqdn)
 mv /etc/httpd/conf.d/$HOST.conf /etc/httpd/conf.d/$HOST.conf.bkp
 sed "s/dataverse.c3.furg.br/$HOST/g" $DIR/conf/dataverse.conf >/etc/httpd/conf.d/$HOST.conf
@@ -23,6 +22,7 @@ sed "s/dataverse.c3.furg.br/$HOST/g" $DIR/conf/httpd.conf >/etc/httpd/conf/httpd
 mv etc/httpd/conf.d/ssl.conf etc/httpd/conf.d/ssl.conf.bkp
 sed "s/dataverse.c3.furg.br/$HOST/g" $DIR/conf/ssl.conf >/etc/httpd/conf.d/ssl.conf
 echo "${GREEN}Generating new SSL Certificates!${RESET}"
+mkdir /etc/httpd/ssl
 $DIR/cert/keygen.sh -y 3 -f -u root -g root -h $HOST -e https://$HOST/
 mv $DIR/sp-cert.pem /etc/httpd/ssl/$HOST.cer
 mv $DIR/sp-key.pem /etc/httpd/ssl/$HOST.key
