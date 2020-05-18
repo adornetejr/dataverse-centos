f8c4f2ea28e105614e4263d3aa0bcb293181e29c
Adornete Jr <adornetejr@gmail.com>
2020-05-18 05:34:13 -0300

    Fix: language support

--- sh/language.sh
+++ sh/language.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-curl http://localhost:8080/api/admin/settings/:Languages -X PUT -d '[{"locale":"en","title":"English"},{"locale":"pt","title":"Português"}]'
+DIR=$PWD
+systemctl start glassfish
+curl http://localhost:8080/api/admin/settings/:Languages -X PUT -d '[{"locale":"en","title":"English"},{"locale":"br","title":"Português"}]'
 sudo -u glassfish mkdir /home/glassfish/langBundles
 /usr/local/glassfish4/glassfish/bin/asadmin create-jvm-options '-Ddataverse.lang.directory=/home/glassfish/langBundles'
-cd $DIR/lang
 mkdir /tmp/languages
+cd $DIR/lang
 cp -R en_US/*.properties /tmp/languages
 cp -R pt_BR/*.properties /tmp/languages
 cd /tmp/languages
