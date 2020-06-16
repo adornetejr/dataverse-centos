8c3006ec5faca1bdfdac863f5f4945a4e3082928
Adornete Jr <adornetejr@gmail.com>
2020-06-15 23:15:56 -0300

    Fix: api examples

--- sh/api.sh
+++ sh/api.sh
@@ -1,4 +1,8 @@
 #/bin/bash
 DIR=$PWD
-curl -H X-Dataverse-key:$1 -X POST http://localhost:8080/api/dataverses/root --upload-file $DIR/json/dataverse-complete.json
+echo "Create Dataverse"
+curl -H "X-Dataverse-key:$1" -X POST "http://localhost:8080/api/dataverses/root" --upload-file $DIR/json/dataverse-complete.json
+echo "Show Dataverse"
 curl http://localhost:8080/api/dataverses/root
+echo "Create Dataset"
+curl -H "X-Dataverse-key:$1" -X POST "http://localhost:8080/api/dataverses/root/datasets" --upload-file "dataset-finch1.json"
