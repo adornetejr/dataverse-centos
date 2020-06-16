336079c40fad8dec57d7bf56b3d8f1a088af91db
Adornete Jr <adornetejr@gmail.com>
2020-06-15 22:28:20 -0300

    Feat: example dataset

--- sh/api.sh
+++ sh/api.sh
@@ -1,2 +1,4 @@
 #/bin/bash
 DIR=$PWD
+curl -H X-Dataverse-key:xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx -X POST http://localhost:8080/api/dataverses/root --upload-file $DIR/json/dataverse-complete.json
+curl http://localhost:8080/api/dataverses/root
